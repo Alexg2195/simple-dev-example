a0a5a1df97a4614d4e7627f384a25830d0101dee
Alexg2195 <alexg2195@gmail.com>
2017-06-05 00:03:37 -0500

    updated example

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
 var User = require('../models/user');
 
 router.get('/', (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // Home Route
 router.get(`*`, function(req, res) {
-  res.send(`Hello!`);
+  res.send(`Hello! Welcome to simple-dev! Start building your api by using "dev create END_POINT_NAME"`);
 });
 
 module.exports = router;
